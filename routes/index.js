8d2c5770b04e590bc2113e879e54f2dd6c682904
justinlewis <j.mapping@gmail.com>
2015-12-22 01:50:55 -0700

    added initial user display

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My Climb Mapper' });
+	var userName = "";
+	if (req.isAuthenticated()){
+		userName = req.user.username;
+	}
+	else {
+		userName = "Example User";
+	}
+  res.render('index', { title: 'My Climb Mapper', username: userName });
 });
 
 exports.users = require('./users');
