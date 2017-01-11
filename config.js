c60440b2fe81c7f51d9593d6eddf897f4db0c38d
Brendon Kay <bak04280@gmail.com>
2017-01-10 23:57:48 -0500

    hacked together hashing for create/update user in users.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.user_name = process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME || 'app_user';
+config.user_name = process.env.OPENSHIFT_POSTGRESQL_DB_USERNAME || 'MAChine';
 config.password = process.env.OPENSHIFT_POSTGRESQL_DB_PASSWORD || 'reader';  
 
 module.exports = config;
--- models/database.js
+++ models/database.js
@@ -1,2 +1,52 @@
-/*var pg = require('pg');
+// From https://www.npmjs.com/package/pg
+var pg = require('pg');
+ 
+// create a config to configure both pooling behavior 
+// and client options 
+// note: all config is optional and the environment variables 
+// will be read if the config is not present 
+var config = {
+  user: 'MAChine', //env var: PGUSER 
+  database: 'climbmapper', //env var: PGDATABASE 
+  password: 'reader', //env var: PGPASSWORD 
+  host: 'localhost', // Server hosting the postgres database 
+  port: 5432, //env var: PGPORT 
+  max: 10, // max number of clients in the pool 
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed 
+};
 
+//this initializes a connection pool 
+//it will keep idle connections open for a 30 seconds 
+//and set a limit of maximum 10 idle clients 
+var pool = new pg.Pool(config);
+ 
+
+// ! This needs to go elsewhere...
+//
+// // to run a query we can acquire a client from the pool, 
+// // run a query on the client, and then return the client to the pool 
+// pool.connect(function(err, client, done) {
+//   if(err) {
+//     return console.error('error fetching client from pool', err);
+//   }
+//   client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+//     //call `done()` to release the client back to the pool 
+//     done();
+ 
+//     if(err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log(result.rows[0].number);
+//     //output: 1 
+//   });
+// });
+ 
+// pool.on('error', function (err, client) {
+//   // if an error is encountered by a client while it sits idle in the pool 
+//   // the pool itself will emit an error event with both the error and 
+//   // the client which emitted the original error 
+//   // this is a rare occurrence but can happen if there is a network partition 
+//   // between your application and the database, the database restarts, etc. 
+//   // and so you might want to handle it and at least log it out 
+//   console.error('idle client error', err.message, err.stack)
+// })
